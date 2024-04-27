b07ad11b01d4189db0a713b07380f9c661084156
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-27 16:42:19 +0900

    section2 > autoslide 구현

---
+++ ACH/static/js/index.js
@@ -0,0 +1,22 @@
+// JavaScript 코드
+window.addEventListener('DOMContentLoaded', function() {
+    const slideWrapper = document.querySelector('.slide-wrapper');
+    const slides = document.querySelectorAll('.slide-wrapper img');
+
+    let counter = 0;
+    const slideWidth = slides[0].clientWidth;
+
+    setInterval(() => {
+        counter++;
+        slideWrapper.style.transition = 'transform 0.5s ease-in-out';
+        slideWrapper.style.transform = `translateX(${-slideWidth * counter}px)`;
+
+        if (counter === slides.length - 1) {
+            setTimeout(() => {
+                slideWrapper.style.transition = 'none';
+                slideWrapper.style.transform = `translateX(0)`;
+                counter = 0;
+            }, 500);
+        }
+    }, 5000); // 이미지를 슬라이드할 시간 간격 (여기서는 2초)
+});
