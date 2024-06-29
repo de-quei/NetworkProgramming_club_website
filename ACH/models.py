c742e874b21a2db9eb47b4128d66e279e3edcf16
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-06-29 14:36:57 +0900

    create model, superuser

--- ACH/models.py
+++ ACH/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.shortcuts import resolve_url
 
-# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=100)  # 게시글의 제목
+    content = models.TextField()  # 게시글의 내용
+    image = models.ImageField(upload_to='posts/%Y/%m/%d/', null=True, blank=True)  # 게시글의 이미지
+    created_at = models.DateTimeField(auto_now_add=True)  # 작성 시간
+    updated_at = models.DateTimeField(auto_now=True)  # 수정 시간
+
+    def __str__(self):  # 객체를 문자열로 표현할 때 사용
+        return f'제목: {self.title}, 작성날짜: {self.created_at}, 수정날짜: {self.updated_at}'
+    def get_absolute_url(self):  # 이 객체의 상세 페이지 URL을 반환
+        return resolve_url('post:detail', pk=self.id)
