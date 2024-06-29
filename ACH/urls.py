071d13072b1966005ad53652b014683f18244b54
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-06-29 14:55:23 +0900

    template에 데이터 출력 (style X)

--- ACH/urls.py
+++ ACH/urls.py
@@ -6,5 +6,6 @@ app_name = 'ACH'
 
 urlpatterns = [
     path('', views.show_index_html, name='index'),
-    path('ai_info/', views.show_ai_info_html, name='ai_info')
+    path('ai_info/', views.show_ai_info_html, name='ai_info'),
+    path('board/', views.BoardListView.as_view(), name='board_list')
 ]
--- ACH/views.py
+++ ACH/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from ACH.models import Board
+
 
 def show_index_html(request):
     return render(request, 'ACH/index.html')
 
 def show_ai_info_html(request):
     return render(request, 'ACH/ai_info.html')
+
+class BoardListView(ListView):
+    model = Board
